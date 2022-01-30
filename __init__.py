39693035729063951468b225e06ef04874f4bc7d
Thiago Nóbrega <thiagonobrega@uepb.edu.br>
2022-01-30 20:38:37 +0000

    pypi package

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = 'phd_dataset_util'
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phd_dataset_util",
+    version="0.1.0",
+    author="Thiago Nóbrega",
+    author_email="thiagonobrega@gmail.com",
+    description="A package that aims to execute the basic dataprocessing from my phd experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiaonobrega/ml_ds_util",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
